17599b955f749e07f5a4c91b3e677ec295e8b57e
Vishwaa-Arumugam <127880126+Vishwaa-Arumugam@users.noreply.github.com>
2024-07-11 14:17:53 +0530

    responsive and animations

--- client-web/src/Components/Customer.jsx
+++ client-web/src/Components/Customer.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 
 const FormInput = ({ id, label, type, value, onChange }) => (
@@ -32,6 +31,7 @@ const FormSelect = ({ id, label, value, onChange, options }) => (
 
 const Customer = () => {
   const [step, setStep] = useState(1); // Track current form step
+  const [animate, setAnimate] = useState(false);
   const [formState, setFormState] = useState({
     customerName: '',
     customerAddress: '',
@@ -62,10 +62,18 @@ const Customer = () => {
     }));
   };
 
+  const nextStep = () => {
+    setAnimate(true); 
+    setTimeout(() => {
+      setStep(step + 1);
+      setAnimate(false); 
+    }, 1000); 
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (step < 3) {
-      setStep(step + 1);
+      nextStep();
     } else {
       const formData = new FormData();
       Object.entries(formState).forEach(([key, value]) => {
@@ -105,12 +113,12 @@ const Customer = () => {
   ];
 
   return (
-    <div className='flex h-screen'>
-      <div className='relative w-2/3'>
+    <div className='md:flex h-[91.4vh]'>
+      <div className='relative md:w-2/3'>
         <img src='/images/lap.jpg' className='w-full brightness-50 h-full object-cover' alt='Background' />
-        <div className='absolute inset-0 flex flex-col items-center justify-center text-center text-white'>
-          <h1 className='text-5xl font-bold font-sans'>Bidding Website</h1>
-          <p className='mt-4 text-lg font-light'>
+        <div className='absolute inset-0 flex flex-col items-center justify-center text-center text-[#cccccc]'>
+          <h1 className='md:text-5xl text-2xl font-bold font-sans'>Bidding Website</h1>
+          <p className='mt-4 md:text-lg text-sm font-light'>
             This is some text on the left side. This is some text on the left side.
             This is some text on the left side. This is some text on the left side.
           </p>
@@ -120,8 +128,8 @@ const Customer = () => {
       <div className='flex flex-col w-full md:w-1/3 bg-[#3F51B5] px-10 py-12'>
         <form onSubmit={handleSubmit} className='flex flex-col gap-8 mt-16'>
           {step === 1 && (
-            <div className='flex flex-col p-5 rounded-lg'>
-              <h2 className='text-3xl font-mono mb-8 text-white'>Customer Details</h2>
+            <div className={`flex flex-col p-5 rounded-lg ${animate ? 'animate-fadeoutleft' : ''}`}>
+              <h2 className='text-3xl font-mono mb-8 text-[#cccccc]'>Customer Details</h2>
               {fields.slice(0, 3).map((field) => (
                 <FormInput
                   key={field.id}
@@ -135,8 +143,8 @@ const Customer = () => {
               <center>
                 <button
                   type='button'
-                  onClick={() => setStep(step + 1)}
-                  className='flex items-center bg-indigo-500 text-white border border-indigo-700 p-2 rounded-xl text-xl w-32 mt-8 transition duration-300 hover:bg-indigo-600'
+                  onClick={nextStep}
+                  className='flex items-center bg-indigo-500 text-[#cccccc] border border-indigo-700 p-2 rounded-xl text-xl w-32 mt-8 transition duration-300 hover:bg-indigo-600'
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -155,8 +163,8 @@ const Customer = () => {
           )}
 
           {step === 2 && (
-            <div className='flex flex-col'>
-              <h2 className='text-3xl font-bold mb-8 text-white'>Company Details</h2>
+            <div className={`flex flex-col p-5 rounded-lg ${animate ? 'animate-fadeinright' : ''}`}>
+              <h2 className='text-3xl font-bold mb-8 text-[#cccccc]'>Company Details</h2>
               <FormSelect
                 id="companyType"
                 label="Company Type"
@@ -177,8 +185,8 @@ const Customer = () => {
               <center>
                 <button
                   type='button'
-                  onClick={() => setStep(step + 1)}
-                  className='flex items-center bg-indigo-500 text-white border border-indigo-700 p-2 rounded-xl text-xl w-32 mt-8 transition duration-300 hover:bg-indigo-600'
+                  onClick={nextStep}
+                  className='flex items-center bg-indigo-500 text-[#cccccc] border border-indigo-700 p-2 rounded-xl text-xl w-32 mt-8 transition duration-300 hover:bg-indigo-600'
                 >
                   Next
                 </button>
@@ -187,8 +195,8 @@ const Customer = () => {
           )}
 
           {step === 3 && (
-            <div className='flex flex-col'>
-              <h2 className='text-3xl font-bold mb-8 text-white'>Contact Details</h2>
+            <div className={`flex flex-col ${animate ? 'animate-fadeinright' : ''}`}>
+              <h2 className='text-3xl font-bold mb-8 text-[#cccccc]'>Contact Details</h2>
               {fields.slice(6, 10).map((field) => (
                 <FormInput
                   key={field.id}
@@ -200,8 +208,8 @@ const Customer = () => {
                 />
               ))}
               <div className='flex flex-col mb-4'>
-                <label htmlFor="upload" className='text-lg font-semibold mb-2 text-white'>Upload your document (GST, MSME, etc..)</label>
-                <label className='flex gap-1 h-12 w-40 mt-2 justify-center bg-transparent border border-gray-300 rounded-2xl px-7 items-center text-xl text-white cursor-pointer hover:border-indigo-500 transition duration-300'>
+                <label htmlFor="upload" className='text-lg font-semibold mb-2 text-[#cccccc]'>Upload your document (GST, MSME, etc..)</label>
+                <label className='flex gap-1 h-12 w-40 mt-2 justify-center bg-transparent border border-gray-300 rounded-2xl px-7 items-center text-xl text-[#cccccc] cursor-pointer hover:border-indigo-500 transition duration-300'>
                   <input multiple type="file" className='hidden' onChange={handleFileChange} />
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
@@ -212,7 +220,7 @@ const Customer = () => {
               <center>
                 <button
                   type='submit'
-                  className='bg-indigo-500 text-white border border-indigo-700 p-2 rounded-xl text-xl w-32 mt-8 transition duration-300 hover:bg-indigo-600'
+                  className='bg-indigo-500 text-[#cccccc] border border-indigo-700 p-2 rounded-xl text-xl w-32 mt-8 transition duration-300 hover:bg-indigo-600'
                 >
                   Submit
                 </button>
--- client-web/tailwind.config.js
+++ client-web/tailwind.config.js
@@ -1,3 +1,4 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -5,7 +6,34 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "fade-in-right": {
+          "0%": {
+            opacity: 0,
+            transform: "translate3d(100%, 0, 0)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate3d(0, 0, 0)",
+          },
+        },
+        "fade-out-left": {
+          "0%": {
+            opacity: 1,
+            transform: "translate3d(0, 0, 0)",
+          },
+          "100%": {
+            opacity: 0,
+            transform: "translate3d(-100%, 0, 0)",
+          },
+        },
+      },
+      animation: {
+        fadeinright: 'fade-in-right 1s ease-in-out',
+        fadeoutleft: 'fade-out-left 1s ease-in-out',
+      },
+    },
   },
   plugins: [],
 }
