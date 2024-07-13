8e9579bc8ffaf19e7474ddb3f3e903a1e148e092
Nandha Krishna <115474356+nandhakrishna10@users.noreply.github.com>
2024-07-13 13:29:12 +0530

    Add files via upload
    Routing for login added

--- client-web/src/App.jsx
+++ client-web/src/App.jsx
@@ -1,42 +1,53 @@
-
-import { useState } from 'react'
-import './App.css'
-import Button from './Components/Button'
+import { useState } from 'react';
+import './App.css';
+import Button from './Components/Button';
 import Customer from './Components/Customer';
 import Vendor from './Components/Vendor';
+import Login from './Components/Login';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:5000'
-axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'http://localhost:5000';
+axios.defaults.withCredentials = true;
 
 function App() {
   const [vendor, setVendor] = useState(false);
   const [customer, setCustomer] = useState(true);
+  const [login, setLogin] = useState(false);
 
   function handleVendor() {
-      setVendor(true);
-      setCustomer(false);
+    setVendor(true);
+    setCustomer(false);
+    setLogin(false);
   }
 
   function handleCustomer() {
-     setCustomer(true);
-     setVendor(false);
+    setCustomer(true);
+    setVendor(false);
+    setLogin(false);
+  }
+
+  function handleLogin() {
+    setLogin(true);
+    setVendor(false);
+    setCustomer(false);
   }
 
   return (
     <>
-    <div className='relative'>
-    <div className='p-5 flex gap-10 justify-center absolute z-10 md:right-36 top-24'>
-        <Button name={"Vendor"} handleClick={() => handleVendor()} />
-        <Button name={"Customer"} handleClick={() => handleCustomer()} />
+      <div className='relative'>
+        <div className='p-5 flex gap-10 justify-center absolute z-10 md:right-36 top-24'>
+          <Button name={"Vendor"} handleClick={handleVendor} />
+          <Button name={"Customer"} handleClick={handleCustomer} />
+          <Button name={"Login"} handleClick={handleLogin} />
+        </div>
       </div>
-    </div>
       <div>
-        {vendor && !customer ? <Vendor/> : ""}
-        {customer && !vendor ? <Customer/> : ""}
+        {vendor && !customer && !login ? <Vendor /> : ""}
+        {customer && !vendor && !login ? <Customer /> : ""}
+        {login && !vendor && !customer ? <Login /> : ""}
       </div>
     </>
-  )
+  );
 }
 
 export default App;
